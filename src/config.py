82508def72f6afa97d75f7f30e2654f317a124a8
OlegSuperBro <olegrakovic323@gmail.com>
2023-07-17 17:42:39 +0300

    refac: changed how config works, added constants

--- src/config.py
+++ src/config.py
@@ -1,27 +1,39 @@
 import yaml
 import inspect
+import contextlib
+import sys
 from dataclasses import dataclass
+from os.path import abspath, dirname
 
-CONFIG_PATH = "config.yaml"
+
+class CONSTANTS:
+    program_path = f"{dirname(abspath(sys.argv[0]))}"
+
+    assets_dir = f"{program_path}\\assets"
+    cache_dir = f"{program_path}\\cache"
+
+    skins_dir = f"{assets_dir}\\Skins"
+
+    config_file = f"{program_path}\\config.yaml"
+    dpg_config_file = f"{program_path}\\dearpygui.ini"
 
 
 @dataclass
-class Config:
-    osu_path: str = "C:/Games/osu!/"
+class CONFIG:
+    osu_path: str = "C:\\Games\\osu!"
+    skin: str = "Default"
 
     @classmethod
     def from_dict(cls, env):
-        return cls(**{
-            k: v for k, v in env.items()
-            if k in inspect.signature(cls).parameters
-        })
+        for key, value in {k: v for k, v in env.items() if k in inspect.signature(cls).parameters}.items():
+            setattr(cls, key, value)
 
 
-try:
-    CONFIG = Config.from_dict(yaml.safe_load(open(CONFIG_PATH, "r")))
-except FileNotFoundError:
-    CONFIG = Config()
+with contextlib.suppress(FileNotFoundError):
+    CONFIG.from_dict(yaml.safe_load(open(CONSTANTS.config_file, "r")))
 
 
 def save():
-    yaml.safe_dump(dict(CONFIG), open(CONFIG_PATH, "w"))
+    yaml.safe_dump({key: CONFIG.__dict__.get(key)
+                    for key in list(CONFIG.__annotations__.keys())},
+                   open(CONSTANTS.config_file, "w"))
