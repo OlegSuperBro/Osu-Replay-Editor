b91d1b726a58c45dd6851ab0c10dcd08f03710f5
OlegSuperBro <olegrakovic323@gmail.com>
2023-07-20 01:26:35 +0300

    feat: reworked lifebar, preview in lifebar editor

--- src/gui/tools/lifegraph/lifegraph.py
+++ src/gui/tools/lifegraph/lifegraph.py
@@ -6,6 +6,7 @@ from dataclasses import dataclass
 import utils
 
 from gui.template import Template
+from config import CONSTANTS
 
 
 @dataclass
@@ -15,6 +16,11 @@ class XYPoint:
 
 
 class LifeBarGraphTab(Template):
+    preview_offset_x = -410
+    preview_offset_y = -20
+    squish = 100
+    # 415x175. Full screen is 4 times wider and 6 time taller
+
     def __init__(self, update_func) -> None:
         self._id = self._build()
 
@@ -25,33 +31,38 @@ class LifeBarGraphTab(Template):
         self.point_moved = None
 
     def _build(self) -> None:
-        def query(sender, app_data, user_data):
-            dpg.set_axis_limits("x_axis", app_data[0], app_data[1])
-            dpg.set_axis_limits("y_axis", app_data[2], app_data[3])
+        with dpg.texture_registry():
+            img = dpg.load_image(f"{CONSTANTS.assets_dir}/song_result_screen.jpg")
+            dpg.add_dynamic_texture(img[0], img[1], img[3], tag="preview_result_screen")
 
         with dpg.item_handler_registry(tag="add_point_click_handler"):
             dpg.add_item_clicked_handler(dpg.mvMouseButton_Right, callback=self.rmb_callback)
 
         with dpg.tab(label="Life Graph", tag="life_window") as _id:
-            with dpg.plot(width=405, height=175, query=True, callback=query, tag="full_life_graph", query_button=dpg.internal_dpg.mvMouseButton_Right):
-                dpg.add_plot_axis(dpg.mvXAxis, label="Ticks", tag="full_x_axis")
-                dpg.add_plot_axis(dpg.mvYAxis, label="Life %", tag="full_y_axis")
-                dpg.set_axis_limits("full_x_axis", 0, 1)
-                dpg.set_axis_limits("full_y_axis", -5, 105)
-                dpg.add_line_series([], [], parent="full_y_axis", tag="full_life_graph_line")
+            with dpg.group(horizontal=True):
+                dpg.add_checkbox(label="Save Mode", default_value=True, tag="save_mode_toggle", callback=self.toggle_save_mode)
+                dpg.add_spacer(width=50)
 
-            with dpg.plot(width=405, height=175, tag="life_graph", context_menu_button=-1, no_title=True):
+                with dpg.group(horizontal=True, tag="save_mode_group"):
+                    pass
+                with dpg.group(horizontal=True, show=False, tag="unsave_mode_group"):
+                    dpg.add_checkbox(label="Show Preview", callback=lambda x: dpg.configure_item("preview_lifebar", show=dpg.get_value(x)))
+
+            with dpg.plot(width=-1, height=-1, tag="life_graph", context_menu_button=-1, no_title=True):
                 dpg.add_plot_axis(dpg.mvXAxis, label="", tag="x_axis", no_tick_labels=True, no_tick_marks=True)
                 dpg.add_plot_axis(dpg.mvYAxis, label="", tag="y_axis", no_tick_labels=True, no_tick_marks=True)
-                dpg.set_axis_limits("x_axis", 0, 1)
-                dpg.set_axis_limits("y_axis", -5, 105)
+
+                with dpg.draw_layer(tag="unsave_group", show=False):
+                    dpg.add_image_series("preview_result_screen", (0, self.preview_offset_y), (0, 600 + self.preview_offset_y), parent="x_axis", tag="preview_lifebar", show=False)
+                    dpg.draw_rectangle(pmin=(0, 0), pmax=(0, 100), color=(255, 0, 0, 255), thickness=20, tag="save_zone_square")
+
                 dpg.add_line_series([], [], parent="y_axis", tag="life_graph_line")
 
             dpg.bind_item_handler_registry("life_graph", "add_point_click_handler")
 
             return _id
 
-    def rmb_callback(self, caller, app_data, user_data) -> None:
+    def rmb_callback(self, caller=None, app_data=None, user_data=None) -> None:
         x, y = dpg.get_plot_mouse_pos()
 
         if x != self.old_x or y != self.old_y:
@@ -71,7 +82,7 @@ class LifeBarGraphTab(Template):
         self.update_all_points()
         self.update_line_plot()
 
-    def point_callback(self, caller, app_info, user_info):
+    def point_callback(self, caller, app_info=None, user_info=None):
         index = int(dpg.get_item_alias(caller).split("_")[1])
         x, y, _, _ = dpg.get_value(caller)
 
@@ -86,6 +97,18 @@ class LifeBarGraphTab(Template):
         self.update_point_list(index, x, y)
         self.update_line_plot()
 
+    def toggle_save_mode(self, caller=None, app_info=None, user_info=None):
+        dpg.configure_item("save_mode_group", show=dpg.get_value("save_mode_toggle"))
+        dpg.configure_item("unsave_mode_group", show=not dpg.get_value("save_mode_toggle"))
+        dpg.configure_item("unsave_group", show=not dpg.get_value("save_mode_toggle"))
+
+    def update_preview(self):
+        dpg.configure_item("save_zone_square", pmin=(0, 0), pmax=(self.lifebar_graph_list[-1:][0].x, 100))
+
+        dpg.configure_item("preview_lifebar",
+                           bounds_min=((self.lifebar_graph_list[-1:][0].x / 415 * (self.preview_offset_x + self.squish / 2)), self.preview_offset_y),
+                           bounds_max=((self.lifebar_graph_list[-1:][0].x / 415 * (1980 + self.preview_offset_x - self.squish / 2)), 600 + self.preview_offset_y))
+
     def read_from_replay(self, replay: Replay):
         lifebar = utils.decrease_lifebar_length(replay.life_bar_graph)
         for _ in range(len(self.lifebar_graph_list) - 1):
@@ -97,8 +120,7 @@ class LifeBarGraphTab(Template):
             self.update_point_plot(index)
 
         self.update_line_plot()
-        dpg.set_axis_limits("full_x_axis", 0, x)
-        dpg.set_axis_limits("x_axis", 0, x)
+        self.update_preview()
 
     def update_point_list(self, index: int, x: int, y: int):
         self.lifebar_graph_list[index].x, self.lifebar_graph_list[index].y = x, y
@@ -123,7 +145,6 @@ class LifeBarGraphTab(Template):
 
     def update_line_plot(self):
         if len(self.lifebar_graph_list) >= 2:
-            dpg.set_value("full_life_graph_line", [[point.x for point in self.lifebar_graph_list], [point.y for point in self.lifebar_graph_list]])
             dpg.set_value("life_graph_line", [[point.x for point in self.lifebar_graph_list], [point.y for point in self.lifebar_graph_list]])
 
     def read_in_replay(self, replay: Replay) -> None:
